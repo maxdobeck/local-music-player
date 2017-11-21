62c1fbaed5bc6d6667cfa135f896f24eee4b9185
dobecma <dobeckmax@gmail.com>
2017-11-20 20:08:22 -0800

    Started Bookmark Browser component.  Testing file reads.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    <landing-page></landing-page>
+    <bookmark-browser></bookmark-browser>
   </div>
 </template>
 
 <script>
-  import LandingPage from '@/components/LandingPage'
+  // import LandingPage from '@/components/LandingPage'
+  import BookmarkBrowser from '@/components/BookmarkBrowser'
 
   export default {
     name: 'local-music-player',
     components: {
-      LandingPage
+      BookmarkBrowser
+      // LandingPage
     }
   }
 </script>
