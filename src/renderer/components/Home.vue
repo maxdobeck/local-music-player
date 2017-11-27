246dced7a25eeb33761ea5af62b40bba4ff89541
dobecma <dobeckmax@gmail.com>
2017-11-27 15:16:17 -0800

    Styled homepage navbar using iview.

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,10 +1,31 @@
 <template>
-  <div>
-    <navbar></navbar>
-    <playlist-queue></playlist-queue>
-    <song-queue></song-queue>
-    <tile-area></tile-area>
-    <currently-playing></currently-playing>
+  <div id="wrapper">
+    <Menu mode="horizontal" theme="dark" active-name="1">
+      <MenuItem name="1">
+        Item 1
+      </MenuItem>
+      <MenuItem name="2">
+        Item 2
+      </MenuItem>
+      <MenuItem name="3">
+        Item 3
+      </MenuItem>
+    </Menu>
+    <div class="topNav">
+      <navbar></navbar>
+    </div>
+    <main>
+      <div class="col1">
+        <playlist-queue></playlist-queue>
+      </div>
+      <div class="col2">
+        <song-queue></song-queue>
+      </div>
+      <div class="col3">
+        <tile-area></tile-area>
+        <currently-playing></currently-playing>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -22,4 +43,33 @@
 </script>
 
 <style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  .topNav {
+
+  }
+
+  main {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .col1 {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .col2 {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .col3 {
+    display: flex;
+    flex-direction: column;
+  }
 </style>
