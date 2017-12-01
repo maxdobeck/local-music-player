eebc3b66046a501cd210afe19373efc5bc3a732d
dobecma <dobeckmax@gmail.com>
2017-11-30 20:16:54 -0800

    Can succesfully reach in and read bookmarks.

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -8,6 +8,16 @@ Vue.use(iView)
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
+let chromeBookmarks = '/home/max/.config/google-chrome/Default/Bookmarks'
+const fs = require('fs')
+let musicFile
+
+fs.readFile(chromeBookmarks, 'utf8', function (err, data) {
+  if (err) throw err
+  bookmarks = JSON.parse(data)
+  console.log(bookmarks.roots.bookmark_bar.children[0].children)
+})
+
 /* eslint-disable no-new */
 new Vue({
   components: { App },
